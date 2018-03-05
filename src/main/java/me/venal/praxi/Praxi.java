c5fafc9783394b977670dfa0a22e25cac694a6c5
Venal <venaldev@gmail.com>
2018-03-04 22:41:19 -0500

    added basic guice layout

--- src/main/java/me/venal/praxi/Praxi.java
+++ src/main/java/me/venal/praxi/Praxi.java
@@ -1,15 +1,22 @@
 package me.venal.praxi;
 
+import com.google.inject.Injector;
 import me.venal.praxi.player.UserHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Praxi extends JavaPlugin {
     
+    private Injector injector;
     private UserHandler userHandler;
+    private PraxiModule praxiModule;
     
     @Override
     public void onEnable() {
         userHandler = new UserHandler(this);
+        praxiModule = new PraxiModule(this);
+        
+        injector = praxiModule.createInjector();
+        injector.injectMembers(this);
     }
 
     @Override
--- src/main/java/me/venal/praxi/PraxiModule.java
+++ src/main/java/me/venal/praxi/PraxiModule.java
@@ -1,11 +1,23 @@
 package me.venal.praxi;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 
 public class PraxiModule extends AbstractModule {
-    
+
+    private final Praxi praxi;
+
+    public PraxiModule(Praxi praxi) {
+        this.praxi = praxi;
+    }
+
     @Override
     protected void configure() {
-        
+        this.bind(Praxi.class).toInstance(praxi);
+    }
+
+     public Injector createInjector() {
+        return Guice.createInjector(this);
     }
 }
